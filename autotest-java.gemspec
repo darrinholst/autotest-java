91c4b14b9fc844c4a553330be0dd12e72e2305be
Darrin Holst <darrinholst@gmail.com>
2009-08-10 08:37:27 -0500

    i love git

---
+++ autotest-java.gemspec
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = %q{autotest-java}
+  s.version = "0.0.1"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["Darrin Holst"]
+  s.date = %q{2009-08-10}
+  s.email = %q{darrinholst@gmail.com}
+  s.extra_rdoc_files = [
+    "LICENSE",
+     "README.rdoc"
+  ]
+  s.files = [
+    ".document",
+     ".gitignore",
+     "LICENSE",
+     "README.rdoc",
+     "Rakefile",
+     "VERSION",
+     "autotest-java.gemspec",
+     "lib/autotest-java.rb",
+     "lib/autotest/discover.rb",
+     "lib/autotest/junit_to_testunit_formatter.rb",
+     "lib/autotest/maven.rb",
+     "test/autotest-java_test.rb",
+     "test/test_helper.rb"
+  ]
+  s.homepage = %q{http://github.com/darrinholst/autotest-java}
+  s.rdoc_options = ["--charset=UTF-8"]
+  s.require_paths = ["lib"]
+  s.rubyforge_project = %q{autotest-java}
+  s.rubygems_version = %q{1.3.5}
+  s.summary = %q{run java tests with autotest}
+  s.test_files = [
+    "test/autotest-java_test.rb",
+     "test/test_helper.rb"
+  ]
+
+  if s.respond_to? :specification_version then
+    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+    s.specification_version = 3
+
+    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    else
+    end
+  else
+  end
+end
--- lib/autotest/maven.rb
+++ lib/autotest/maven.rb
@@ -1,9 +1,48 @@
 require 'autotest'
 
 class Autotest::Maven < Autotest
+  attr_accessor :classpath
+  
   def initialize # :nodoc:
     super
+    cache_classpath
+    add_mappings
 
+    self.failed_results_re = /^\d+\) (.*)\((.*)\)/
+  end
+  
+  def make_test_cmd files_to_test
+    cmd = ""
+    classes = reorder(files_to_test).map{|k,v| file_to_class(k)}
+    
+    unless(classes.empty?)
+      cmd = "#{java} -classpath \"#{classpath}\" #{test_runner} #{classes.join(' ')} | #{results_formatter}"
+    end
+    
+    cmd
+  end
+  
+  def extra_class_map
+    Hash[
+      *self.find_order.grep(/^target\/test-classes/).map { |f|
+        [path_to_classname(f), f]
+      }.flatten
+    ]    
+  end
+  
+  def path_to_classname(path)
+    path.gsub(/.*classes\//, '').gsub(/\.class/, '').gsub(/\//, '.')
+  end
+  
+  def cache_classpath
+    puts "extracting classpath from the maven, please stand by"
+    result = `mvn -o dependency:build-classpath`
+    match = result.match(/.*\.jar/)
+    raise "Unable to glean classpath from #{result}" unless match
+    self.classpath = ['target/classes', 'target/test-classes', match[0]].join(File::PATH_SEPARATOR) if match
+  end
+  
+  def add_mappings
     clear_mappings
 
     add_mapping(/^target\/classes\/.*\.class$/) do |filename, match|
@@ -16,12 +55,19 @@ class Autotest::Maven < Autotest
     end
   end
   
-  def make_test_cmd files_to_test
-    cmd = ""
-    files = reorder(files_to_test).keys
-    
-    unless(files.empty?)
-    end
-    
-    cmd
+  def java
+    "java"
+  end
+  
+  def test_runner
+    "org.junit.runner.JUnitCore"
+  end
+  
+  def file_to_class(file)
+    file.gsub(/.*classes\//, '').gsub(/\.class/, '').gsub(/\//, '.')
+  end
+  
+  def results_formatter
+    "ruby #{File.join(File.dirname(__FILE__), 'junit_to_testunit_formatter.rb')}"
+  end
 end
